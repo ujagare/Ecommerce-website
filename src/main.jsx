bcaa55f94877f31d2db7f8cc64f6a96cb76c93d6
Sunny Ujagare <ujagare@gmail.com>
2025-01-29 17:13:59 +0530

    Configure GitHub Pages deployment

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import Context from './Utils/Context.jsx'
 
 createRoot(document.getElementById('root')).render(
   <Context>
-  <BrowserRouter>
+  <HashRouter>
     <App />
-  </BrowserRouter>
+  </HashRouter>
   </Context>
 )
